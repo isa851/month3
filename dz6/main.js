7f9b71dab7ebdf7c97446be2daba9afe154d47a1
isa851 <isa904363@gmail.com>
2025-10-25 16:34:50 +0600

    fix

---
+++ dz6/main.js
@@ -0,0 +1,15 @@
+const accordionItems = document.querySelectorAll(".accordion-item");
+
+accordionItems.forEach(item => {
+  const header = item.querySelector(".accordion-header");
+
+  header.addEventListener("click", () => {
+    const openItem = document.querySelector(".accordion-item.active");
+
+    if (openItem && openItem !== item) {
+      openItem.classList.remove("active");
+    }
+
+    item.classList.toggle("active");
+  });
+});
